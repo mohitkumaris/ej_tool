679011563afd07aa7fce4503d54b0c3101122949
Mohit Kumar <mohitkumaris@gmail.com>
2024-01-31 15:53:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+//import axios from "axios";
+import "./App.css";
+
+function App() {
+  // const baseUrl = "http://localhost:8081/rest";
+  // const handleClick = (e) => {
+  //   e.preventDefault();
+  //   postData(baseUrl);
+  // };
+  // const postData = async (baseUrl) => {
+  //   const response = await axios.post(baseUrl + "/save", {
+  //     name: "Sending the complex data and this is latest",
+  //     email: "mohitkumar@emal.com",
+  //     age: "39",
+  //     city: "Delhi",
+  //     faid: "test",
+  //   });
+  //   console.log(response.data);
+  //   window.open("http://localhost:3000/");
+  //}
+
+  return (
+    <>
+      <p>This is testing app</p>
+    </>
+  );
+}
+export default App;
